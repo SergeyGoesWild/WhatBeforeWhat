c9e03f5e4fa66643711f9e0408d9aabe61fe9566
Sergey Telnov <sergej.telnov@gmail.com>
2025-03-04 08:54:14 +0100

    Modified "data base" for educational content, wip

--- WhatBeforeWhat/DataProvider.swift
+++ WhatBeforeWhat/DataProvider.swift
@@ -12,6 +12,7 @@ struct HistoricItem {
     let picture: String
     let date: Int
     let flavourText: String
+    let circa: Bool
 }
 
 final class DataProvider {
@@ -22,20 +23,20 @@ final class DataProvider {
     private var prevIndex2: Int?
     
     let data: [HistoricItem] = [
-        HistoricItem(id: UUID(), picture: "HandsCave", date: -25000, flavourText: "XXX"),
-        HistoricItem(id: UUID(), picture: "BlackSquare", date: 1915, flavourText: "XXX"),
-        HistoricItem(id: UUID(), picture: "ElCastillo", date: 1100, flavourText: "XXX"),
-        HistoricItem(id: UUID(), picture: "MonaLisa", date: 1506, flavourText: "XXX"),
-        HistoricItem(id: UUID(), picture: "NotreDame", date: 1345, flavourText: "XXX"),
-        HistoricItem(id: UUID(), picture: "Parthenon", date: -432, flavourText: "XXX"),
-        HistoricItem(id: UUID(), picture: "SacreCoeur", date: 1923, flavourText: "XXX"),
-        HistoricItem(id: UUID(), picture: "SantaMarieDelFiore", date: 1436, flavourText: "XXX"),
-        HistoricItem(id: UUID(), picture: "StarryNight", date: 1889, flavourText: "XXX"),
-        HistoricItem(id: UUID(), picture: "GreatWaveOffKanagawa", date: 1831, flavourText: "XXX"),
-        HistoricItem(id: UUID(), picture: "GirlPearlEarring", date: 1665, flavourText: "XXX"),
-        HistoricItem(id: UUID(), picture: "Scream", date: 1893, flavourText: "XXX"),
-        HistoricItem(id: UUID(), picture: "Petra", date: 40, flavourText: "XXX"),
-        HistoricItem(id: UUID(), picture: "EiffelTower", date: 1889, flavourText: "XXX"),
+        HistoricItem(id: UUID(), picture: "HandsCave", date: -25000, flavourText: "Prehistoric hand paintings at the Cave of Hands in Argentina.", circa: true),
+        HistoricItem(id: UUID(), picture: "BlackSquare", date: 1915, flavourText: "'Black square' painting by Kasimir Malevitch, considered one of the most radical artworks of its time.", circa: false),
+        HistoricItem(id: UUID(), picture: "ElCastillo", date: 1100, flavourText: "Temple of Kukulcan - a Mesoamerican step-pyramid in Mexico, built by the Maya civilization.", circa: true),
+        HistoricItem(id: UUID(), picture: "MonaLisa", date: 1506, flavourText: "'Mona Lisa' a painting by Leonardo da Vinci, an archetypal masterpiece of the Italian Renaissance.", circa: false),
+        HistoricItem(id: UUID(), picture: "NotreDame", date: 1345, flavourText: "Notre-Dame is a famous Gothic cathedral in Paris, France.", circa: false),
+        HistoricItem(id: UUID(), picture: "Parthenon", date: -432, flavourText: "The Parthenon is an ancient Greek temple located on the Acropolis of Athens.", circa: true),
+        HistoricItem(id: UUID(), picture: "SacreCoeur", date: 1923, flavourText: "Sacré-Cœur is a renowned Roman Catholic church located at the summit of Montmartre, Paris.", circa: false),
+        HistoricItem(id: UUID(), picture: "SantaMarieDelFiore", date: 1436, flavourText: "Santa Maria del Fiore is a famous church and a masterpiece of Italian Gothic and Renaissance architecture.", circa: false),
+        HistoricItem(id: UUID(), picture: "StarryNight", date: 1889, flavourText: "'Starry Night' is one of the most famous paintings in the world, created by Vincent van Gogh in 1889", circa: false),
+        HistoricItem(id: UUID(), picture: "GreatWaveOffKanagawa", date: 1831, flavourText: "'The Great Wave off Kanagawa' is a Japanese woodblock print by Katsushika Hokusai, one of the most recognizable works of Japanese art", circa: false),
+        HistoricItem(id: UUID(), picture: "GirlPearlEarring", date: 1665, flavourText: "'Girl with a Pearl Earring' is a painting by Johannes Vermeer during the Dutch Golden Age.", circa: false),
+        HistoricItem(id: UUID(), picture: "Scream", date: 1893, flavourText: "'The Scream' is one of the most famous paintings in art history, created by Edvard Munch in 1893.", circa: false),
+        HistoricItem(id: UUID(), picture: "Petra", date: 40, flavourText: "Petra is an ancient city in Jordan. It is one of the New Seven Wonders of the World.", circa: true),
+        HistoricItem(id: UUID(), picture: "EiffelTower", date: 1889, flavourText: "The Eiffel Tower is a famous landmark symbolizing Paris and France. It was originally built for the 1889 World’s Fair", circa: false),
         HistoricItem(id: UUID(), picture: "Colosseum", date: 80, flavourText: "XXX"),
         HistoricItem(id: UUID(), picture: "TajMahal", date: 1631, flavourText: "XXX"),
         HistoricItem(id: UUID(), picture: "SaintBasilCathedral", date: 1555, flavourText: "XXX"),
